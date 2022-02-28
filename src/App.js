c04ee88134d9a924681f770c70411399128a7abf
Lillian Paris <lillian.paris529@gmail.com>
2022-02-28 12:35:50 -0700

    Added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "../src/Components/Navbar/Navbar";
 import Landing from "../src/Pages/Landing/Landing";
+import Blog from "../src/Pages/Blog/Blog"
 // import BasicSpeedDial from "../src/Components/SpeedDial/SpeedDial";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Landing />} />
+          <Route exact path="/" element={<Blog />} />
         </Routes>
       </Router>
 
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import BasicSpeedDial from '../../Components/SpeedDial/SpeedDial';
-import ReactTyped from "../../Components/ReactTyped/ReactTyped"
+import ReactTyped from "../../Components/ReactTyped/ReactTyped";
+
 
 const Landing = () => {
     return (
